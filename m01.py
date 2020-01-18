bc20758ac4d441dfdde01a9280d0881859cbd459
marco0415 <60027114+marco0415@users.noreply.github.com>
2020-01-18 16:45:40 +0800

    Add files via upload

---
+++ m01.py
@@ -0,0 +1,10 @@
+# Add your Python code here. E.g.
+from microbit import *
+
+
+while True:
+    display.scroll('Hello, World!')
+    display.show(Image.HEART)
+    sleep(2000)
+    display.show(Image.HEART_SMALL)
+    sleep(2000)
